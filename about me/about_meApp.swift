9dbc8677e9f51dbe79bd09a7b20352727ead6b3b
trisha <unipegachicken27@gmail.com>
2023-06-24 10:51:14 -0400

    Initial Commit

---
+++ about me/about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_meApp.swift
+//  about me
+//
+//  Created by Trisha Chakraborty on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
